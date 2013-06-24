003ff989f7616fb176f3efc09261cc2428aedd82
Chris Fennell <chris@springlabdesign.com>
2013-06-24 16:09:42 -0400

    Tested tab template, Coded jCarousel template

--- js/app.js
+++ js/app.js
@@ -12,5 +12,43 @@ $(function () {
 		function () { clearTimeout(ddT); $('#nav-dropdown').stop().fadeIn(); }, 
 	  function () { ddT = setTimeout(function() { $('#nav-dropdown').stop().fadeOut(); }, 500); }
 	);
+
+	var bestsellers_carousel = $('#books-bestsellers .jcarousel').jcarousel({
+    // Core configuration goes here
+	});
+
+	$('#books-bestsellers .jcarousel-prev').jcarouselControl({
+      target: '-=1',
+      carousel: $('#books-bestsellers')
+  });
+
+  $('#books-bestsellers .jcarousel-next').jcarouselControl({
+      target: '+=1',
+      carousel: $('#books-bestsellers')
+  });
+
+  $('#books-bestsellers .jcarousel').jcarouselAutoscroll({
+    'interval': 8000,
+    'autostart': true
+	});
+
+	var top10_carousel = $('#books-top10 .jcarousel').jcarousel({
+    // Core configuration goes here
+	});
+
+	$('#books-top10 .jcarousel-prev').jcarouselControl({
+      target: '-=1',
+      carousel: $('#books-top10')
+  });
+
+  $('#books-top10 .jcarousel-next').jcarouselControl({
+      target: '+=1',
+      carousel: $('#books-top10')
+  });
+
+  $('#books-top10 .jcarousel').jcarouselAutoscroll({
+    'interval': 8000,
+    'autostart': true
+	});
 	
 });
